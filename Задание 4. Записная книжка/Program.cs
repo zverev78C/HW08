a8b425cdb906b57546817b9b81e5eb2d3c1aa35e
Zverev 78 <9842905@gmail.com>
2023-09-09 15:20:08 +0300

    Добавил проект 4 записная книжка

---
+++ Задание 4. Записная книжка/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_4.Записная_книжка
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
