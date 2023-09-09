965e941c394f183f4c93cd7d23306840c04dbd71
Zverev 78 <9842905@gmail.com>
2023-09-09 15:10:58 +0300

    Добавьте файлы проекта.

---
+++ Задание_1.Работа_с_листом/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_1.Работа_с_листом
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            #region Задание 1. Работа с листом
+            /*
+            Что нужно сделать
+            Создайте лист целых чисел. 
+            Заполните лист 100 случайными числами в диапазоне от 0 до 100. 
+            Выведите коллекцию чисел на экран. 
+            Удалите из листа числа, которые больше 25, но меньше 50. 
+            Снова выведите числа на экран. 
+            Рекомендация
+            Сделайте отдельные методы для заполнения, удаления и вывода на экран.
+
+            Что оценивается
+            В программе используется три отдельных метода. 
+            В качестве хранилища данных используется List.
+             */
+            #endregion
+
+            List<int> nums = new List<int>();
+            Random rnd = new Random();
+
+            for (int i = 0; i < 100; i++) // Заполнение списка случайными числами
+            {
+                nums.Add(Num());
+            }
+
+            Print(nums); // Вывод полного списка на экран
+            nums = DeleteNums(nums); // удаление из списка чисел
+            Print(nums); // Вывод отредактированого списка на экран
+            Console.ReadKey();
+
+            #region Методы 
+            // метод рандомного заполнения колекции
+            int Num()
+            {
+                return rnd.Next(0, 100);
+            }
+            // метод удаления из списка чисел 25-50
+            List<int> DeleteNums(List<int> list)
+            {
+                for (int i = 0; i < nums.Count; i++)
+                {
+                    if (nums[i] > 24 && nums[i] < 51) { nums.Remove(nums[i]); i--; }
+                }
+                return list;
+            }
+            // Метод вывода на экран списка
+            void Print(List<int> list)
+            {
+                for (int i = 0; i < list.Count; i++)
+                {
+                    Console.Write($"{list[i].ToString()} ");
+                }
+                Console.WriteLine($"\n\nДлина коллекции: {nums.Count}");
+            }
+            #endregion
+        }
+    }
+}
---
+++ Задание_1.Работа_с_листом/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Задание_1.Работа_с_листом")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Задание_1.Работа_с_листом")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("eefc1fc7-8475-4aaf-bd6d-51d19e970011")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
