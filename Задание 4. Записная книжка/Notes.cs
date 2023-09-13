36f714ec097cbbf93762ec202ca795d9352031cd
Zverev78C <9842905@gmail.com>
2023-09-13 09:43:48 +0300

    создал класс для данных пользовотеля

---
+++ Задание 4. Записная книжка/Notes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_4.Записная_книжка
+{
+    internal class Notes
+    {
+        /*
+        ФИО
+        Улица
+        Номер дома
+        Номер квартиры
+        Мобильный телефон
+        Домашний телефон
+        */
+        string FIO { get; set; }
+        string street { get; set; }
+        string House { get; set; }
+        string flate { get; set; }
+        double mobile { get; set; }
+        double phone { get; set; }
+
+    }
+}
