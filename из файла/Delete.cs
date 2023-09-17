bc1ffcd968fb493f78ab7dabfd8033efdc5dd597
Zverev78C <9842905@gmail.com>
2023-09-17 20:03:37 +0300

    редактирование сотрудника

--- из файла/Delete.cs
+++ из файла/Delete.cs
@@ -12,7 +12,9 @@ namespace из_файла
     class Foundat
     {
         /*
+         Просмотр всех сотрудников      PrintAllWorkers()
          Создание сотрудника            AddWorkers()
+         Редактирование сотрудника      EditWorker()
          * 
          * 
          Просмотр отделов               Print()
@@ -20,7 +22,8 @@ namespace из_файла
          Редактирование отдела          EditDept()
          Удаление отдела                DeleteDept()
          *
-
+         *
+         Сохранение в 
           */
 
         /// <summary>
@@ -103,13 +106,12 @@ namespace из_файла
 
             Deps.Add(new Delete(Name = name, RegDate = DateTime.Now, wrks = depList));
         }
-
         /// <summary>
         /// Метод добовляющий нового сотрудника при создании в соответствующий список отдела 
         /// </summary>
         /// <param name="newWorker"></param>
         /// <param name="deptName"></param>
-        public void ChangeDepsWorker(Works newWorker, string deptName)
+        public void ChangeDepsWorkerAdd(Works newWorker, string deptName)
         {
             for (int i = 0; i < Deps.Count; i++)
             {
@@ -119,7 +121,20 @@ namespace из_файла
                 }
             }
         }
-
+        /// <summary>
+        /// Метод удаления сотрудника из списка отдела 
+        /// </summary>
+        /// <param name="deptName"></param>
+        public void ChangeDepsWorkerDel(string deptName)
+        {
+            for (int i = 0; i < Deps.Count; i++)
+            {
+                if (Deps[i].Name == deptName)
+                {
+                    Deps[i].wrks = Wrks.DepsList(deptName); // просто заменяет старый список сотрудников новым без переведеного сотрудника
+                }
+            }
+        }
         /// <summary>
         /// Метод редактирующий название отдела 
         /// </summary>
@@ -143,7 +158,6 @@ namespace из_файла
             }
             Print();
         }
-
         /// <summary>
         /// Метод выводящий на экран все отделы с индексом
         /// </summary>
@@ -155,7 +169,6 @@ namespace из_файла
                 Console.WriteLine($"{i}  {Deps[i].Name}\t\t{Deps[i].RegDate:D}\t\t{Deps[i].wrks.Count} ");
             }
         }
-
         /// <summary>
         /// Метод условного удаления отдела 
         /// </summary>
@@ -183,7 +196,6 @@ namespace из_файла
                 }
             }
         }
-
         #endregion
     }
 
@@ -230,6 +242,18 @@ namespace из_файла
 
         #region Методы  
 
+
+        /// <summary>
+        /// Метод вывода на экран списка всех сотрудников 
+        /// </summary>
+        public void PrintAllWorkers()
+        {
+            PrintTitle();
+            for (int i = 0; i < workrs.Count; i++) 
+            {
+                Console.WriteLine(workrs[i].Print());
+            }
+        }
         /// <summary>
         /// Метод добавления сотрудника в список
         /// </summary>
@@ -257,10 +281,58 @@ namespace из_файла
                                           Solary = solary,
                                           CountProject = projects);
             workrs.Add(tempWorker);
-            Dlt.ChangeDepsWorker(tempWorker, workDept);
+            Dlt.ChangeDepsWorkerAdd(tempWorker, workDept);
         }
+        /// <summary>
+        /// Метод редактирование сотрудника 
+        /// </summary>
+        public void EditWorker()
+        {
+            PrintAllWorkers();
+            Console.WriteLine("Введите ID работника:");
+            int id = Convert.ToInt32( Fdt.InputCheck(true,true));
 
+            Console.WriteLine("Введите следющую информацию о работнике: ");
 
+            Console.WriteLine($"Существующее Имя: {workrs[id].FisrtName}.\t Новое имя: ");
+            string firstName = Fdt.InputCheck(false, false);                    // допускается пустая строка для сохранения старого имени
+            if (firstName == "") { firstName = workrs[id].FisrtName; }
+            Console.WriteLine($"Существующая Фамилия: {workrs[id].LastName}.\t Новая Фамилия: ");
+            string lastName = Fdt.InputCheck(false, false);
+            if (lastName == "") { lastName = workrs[id].LastName; }
+            Console.WriteLine($"Существующий возраст: {workrs[id].Age}.\\t Новый возраст: ");
+            string age = Fdt.InputCheck(false, true);
+            int tempAge;
+            if (age == "") { tempAge = workrs[id].Age; } else { tempAge = Int32.Parse(age); }
+            Console.WriteLine($"Существующий отдел: {workrs[id].WorkDept}.\\t Новый отдел: ");
+            string dept = Fdt.InputCheck(false, false);
+            string oldDep = "";                                       //сохраняем старое название отдела для редактирования отделов
+            if (dept == "") { dept = workrs[id].WorkDept; } else { oldDep = workrs[id].WorkDept; }
+            Console.WriteLine($"Существующая зарплата: {workrs[id].Solary}.\\t Новая зарплата: ");
+            int tempSolary;
+            string solary = Fdt.InputCheck(false, true);
+            if (solary == "") { tempSolary = workrs[id].Solary; } else { tempSolary = Int32.Parse(solary); }
+            Console.WriteLine($"Существующие проекты: {workrs[id].CountProject}.\\t Изменить кол-во проектов: ");
+            int tempPrj;
+            string projects = Fdt.InputCheck(false, true);
+            if (projects == "") { tempPrj = workrs[id].CountProject; } else { tempPrj = Int32.Parse(projects); }
+
+            Works tempWorker = new Works(workrs[id].WorkID,
+                                              firstName,
+                                              lastName,
+                                              tempAge,
+                                              dept,
+                                              tempSolary,
+                                              tempPrj);
+            workrs[id] = tempWorker;
+            if (dept != "") // при изменении отдела сотрудника
+            {
+                Dlt.ChangeDepsWorkerAdd(tempWorker, dept); // добавляется в новый отдел
+                Dlt.ChangeDepsWorkerDel( oldDep); // удаляется из старого
+            }
+
+           Console.WriteLine(workrs[id].Print());
+        }
 
         /// <summary>
         ///  метод выводящий на экран сотрудников отдела
